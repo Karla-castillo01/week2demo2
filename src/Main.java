6bbfca2a98602e9ad2a16154c8a4d7d6ed9e2a16
Karla Castillo <karlacastbg03@gmail.com>
2025-04-14 14:49:25 -0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //what we have
+        String fullName = "Karla Castillo Garcia";
+
+        //what we want
+        //    String nameList={"Karla", "Castillo", "Garcia"};
+
+        //how? split!
+        String[] nameList = fullName.split(" "); //" "(space) is the delimiter (aka place we want to cut it up, spaces in a sentence, where we cut apart: parse)
+
+        for(String n: nameList){
+            System.out.println(n);
         }
+
+        String[] fruit = {"Apple", "Banana", "Cherry"};
+        String text = String.join(" ", fruit); // glue together
+        System.out.println(text);
+
+        //fast but not too weird or sophisticated
+        StringBuilder sb = new StringBuilder();
+        sb.append("first");
+        sb.append(" ");
+        sb.append("second");
+        sb.append(" ");
+        sb.append("third");
+        System.out.println(sb);
+
+        // slow but familiar
+//        String body1 = "";
+//        body1 += "first";
+//        body1 += "second";
+//        body1 += "third";
+//        System.out.println(body1);
+//
+//        // awkward but faster choice
+//        ArrayList<String> body = new ArrayList<>();
+//        body.add("first");
+//        body.add("second");
+//        body.add("third");
+//        System.out.println(String.join(" ", body));
     }
 }
